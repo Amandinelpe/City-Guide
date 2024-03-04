7dba3dc67950c2fac57eaa06683bbd3a83466409
Amandine <105013857+Amandinelpe@users.noreply.github.com>
2024-03-04 21:31:52 +0100

    Update CustomInputForm.tsx

--- app/src/components/common/CustomInputForm.tsx
+++ app/src/components/common/CustomInputForm.tsx
@@ -1,18 +1,23 @@
-type CustomInputFormProps = {
+import React, { forwardRef } from 'react';
+
+interface CustomInputFormProps {
   type: string;
   label: string;
-};
+}
 
-const CustomInputForm = ({ type, label }: CustomInputFormProps) => {
-  return (
-    <div className="flex flex-col">
-      <label className="text-black text-sm">{label}</label>
-      <input
-        type={type}
-        className="border border-blue rounded-md w-full h-8 text-base px-2"
-      />
-    </div>
-  );
-};
+const CustomInputForm = forwardRef<HTMLInputElement, CustomInputFormProps>(
+  ({ type, label }, ref) => {
+    return (
+      <div className="flex flex-col">
+        <label className="text-black text-sm">{label}</label>
+        <input
+          ref={ref}
+          type={type}
+          className="border border-blue rounded-md w-full h-8 text-base px-2"
+        />
+      </div>
+    );
+  }
+);
 
 export default CustomInputForm;
